6d0cb6ed59dc13438c6f81282d8a3d2c6ba091f4
Kamil Lelonek <squixy.sln@gmail.com>
2015-05-18 22:56:03 +0200

    Fixed specs to conform new API

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,17 +1,14 @@
 import org.specs2.mutable._
 import org.specs2.runner._
+import org.specs2.matcher._
+
 import org.junit.runner._
 
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
 @RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends Specification with JsonMatchers {
 
   "Application" should {
 
@@ -20,11 +17,11 @@ class ApplicationSpec extends Specification {
     }
 
     "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
+      val api = route(FakeRequest(GET, "/")).get
 
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      status(api)          must equalTo(OK)
+      contentType(api)     must beSome.which(_ == "application/json")
+      contentAsString(api) must /("root" -> "/")
     }
   }
 }
